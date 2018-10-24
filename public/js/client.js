bd98d70bf8920fb8b18af94529cbc4cb42ca667d
Arthuchaut <arthur.audebrand@gmail.com>
2018-10-24 22:47:35 +0200

    Added trigger box and the 3d rendered

--- public/js/client.js
+++ public/js/client.js
@@ -9,17 +9,32 @@ import { HTTP } from './lib/http.js'
 
 window.onload = () => {
     const mobile = new Device(Gyroscope, Camera, Loc)
+    const maxDegree = 30
     var curLat = 0
     var curLon = 0
     var debugObject = document.querySelector('#debugger #object')
     var debugLat = document.querySelector('#debugger #lat')
     var debugLon = document.querySelector('#debugger #lon')
+    var triggerBox = document.querySelector('#trigger #triggerContent')
     
     mobile.Camera.watch()
-    // mobile.Gyroscope.trace(axes => console.log(axes))
+
+    mobile.Gyroscope.trace(axes => {
+        let rotateY = 0
+
+        if (axes.x < -maxDegree) rotateY = -maxDegree
+        else if (axes.x > maxDegree) rotateY = maxDegree
+        else rotateY = axes.x
+
+        triggerBox.style.transform = 'rotateY(' + rotateY + 'deg)'
+    })
 
     HTTP.get('/locations', locs => {
         mobile.Loc.trace(pos => {
+            let trigger = document.querySelector('#trigger')
+            let label = document.querySelector('#trigger #label')
+            let describe = document.querySelector('#trigger #describe')
+
             curLat = pos.lat
             curLon = pos.lon
 
@@ -31,10 +46,16 @@ window.onload = () => {
             for (let loc of locs) {
                 if (mobile.Loc.distance(pos.lat, pos.lon, loc.lat, loc.lon) < 12) {
                     console.log(`Locations matched with : ${loc.label}`)
-                    debugObject.innerHTML = `object: <span style="color:#19c059">${loc.label}</span>` 
+                    debugObject.innerHTML = `object: <span style="color:#19c059">${loc.label}</span>`
+
+                    label.textContent = loc.label
+                    describe.textContent = loc.describe
+                    trigger.style.opacity = 1;
+
                     break
                 }
-    
+                
+                trigger.style.opacity = 0;
                 debugObject.innerHTML = 'object: <span style="color:#ffffff6e">matching ...</span>'
             }
         })
